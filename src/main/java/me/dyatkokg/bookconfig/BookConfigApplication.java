1a058850f48d4bf7cbe488baa43377a457d6c06d
dyatkokg <dyatkokg@gmail.com>
2022-04-25 11:49:47 +0400

    first commit

---
+++ src/main/java/me/dyatkokg/bookconfig/BookConfigApplication.java
@@ -0,0 +1,15 @@
+package me.dyatkokg.bookconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class BookConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dyatkokg/bookconfig/BookConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dyatkokg.bookconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
